c9e89ed11bf73d95e7b29dbbb6000af3655849ca
fadilzhar <fadilathazharif@gmail.com>
2024-01-22 11:06:23 +0700

    First

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var Typed = new Typed('#multiple-lines',{
+    strings : ['Islamic Boarding School','Pencetak Pemimpin Rabbani','Calon Pemimpin Negeri'],
+    typeSpeed : 35,
+    backSpeed:35,
+    backDelay:100,
+    loop:true,
+})
